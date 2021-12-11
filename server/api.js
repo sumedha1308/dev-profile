e31c10229384abe2d737b28be09ab50fbbf50a72
Sumedha Deshpande <sumedhasd1308@gmail.com>
2021-12-12 00:17:24 +0530

    Added port into api.js

--- server/api.js
+++ server/api.js
@@ -1,5 +1,16 @@
+/* eslint-disable no-console */
 const express = require('express');
 const developers = require('./developers');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use('/', express.static('public'));
+
+// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`Server listening at port: ${port}`);
+});
 
 const router = express.Router();
 
